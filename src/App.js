390e192e7e31db9ee8e89145e09c7053a3a8c250
Mukund Dholariya <mukunddtridhyatech@gmail.com>
2024-08-14 16:19:58 +0530

    change fn name

--- src/App.js
+++ src/App.js
@@ -42,14 +42,14 @@ function App() {
     flow: "auth-code",
     scope: ["https://www.googleapis.com/auth/calendar.readonly"],
     onSuccess: async (response) => {
-      await getCalendarEvent(response?.code);
+      await oauth2callback(response?.code);
     },
     onError: (errorResponse) => {
       console.log("errorResponse", errorResponse);
     },
   });
 
-  const getCalendarEvent = async (code) => {
+  const oauth2callback = async (code) => {
     try {
       setEvent([]);
       const response = await axios.post(
@@ -60,7 +60,6 @@ function App() {
       );
 
       if (response.status) {
-        setUser(response?.data?.data);
         localStorage.setItem("user", JSON.stringify(response?.data?.data));
         fetchCalendarEvent(response?.data?.data?.email, startDate, endDate);
       }
@@ -105,23 +104,6 @@ function App() {
     if (!!user?.email) fetchCalendarEvent(user?.email, startDate, endDate);
   };
 
-  const handleAddCalendarId = async () => {
-    try {
-      if (!newCalendarId) return;
-
-      const response = await axios.post(
-        "http://localhost:3001/auth/add-calendar-id",
-        { email: user?.email, newCalendarId }
-      );
-      if (response?.data?.status) {
-        setNewCalendarId("");
-        setUser(response?.data?.data);
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
   return (
     <>
       <div className="calendar-container">
@@ -151,31 +133,8 @@ function App() {
         {user?.email && (
           <>
             <div className="calendar-config">
-              <h3>Add New Calendar ID</h3>
               <div className="user-info">
                 <div>Login User: {user?.email}</div>
-                <div>
-                  Calendar ID:
-                  {user?.calendarId?.length !== 0 &&
-                    user?.calendarId?.map((calendarId, index) => (
-                      <span key={index} className="calendar-id">
-                        {calendarId},
-                      </span>
-                    ))}
-                </div>
-              </div>
-              <div className="new-calendar-id">
-                <label>New Calendar ID:</label>
-                <input
-                  type="text"
-                  value={newCalendarId}
-                  onChange={(e) => setNewCalendarId(e.target.value)}
-                  placeholder="Enter new calendar ID"
-                  className="input-field"
-                />
-                <button onClick={handleAddCalendarId} className="btn btn-add">
-                  Add Calendar ID
-                </button>
               </div>
             </div>
             <div className="fetch-event">
